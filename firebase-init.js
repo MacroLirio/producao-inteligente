c2ad8dcc035052c049a9eb0a9c12c8a31805681f
MacroLirio <liriobrancocultural@gmail.com>
2025-08-28 17:26:31 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,10 @@
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDOKPN2dTMWQnc6t_rgjI7zEXo5JH3Rl7w",
+  authDomain: "producao-inteligente.firebaseapp.com",
+  projectId: "producao-inteligente",
+  storageBucket: "producao-inteligente.firebasestorage.app",
+  messagingSenderId: "1096266396160",
+  appId: "1:1096266396160:web:331b360dcb181d9e9f1ee5",
+  measurementId: "G-ZQXETD8840"
+};
